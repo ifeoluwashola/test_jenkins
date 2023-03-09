0761c53522688d3f2c766609a304579293dba96d
ifeoluwashola <dloveade@gmail.com>
2023-03-09 19:30:00 +0100

    updated

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from flask import Flask
+from Inventory_data import *
+from config_data import *
+from default_metadata_chunk_zip import *
+from os.path import exists
+from data_retrieval import *
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run()
+# today=date.today()
+today="2022-05-01"
+baseDir='./Output'
+n=5
+chunksize=1
 
 
+# download_zip_file(today)
+if exists(f"./Input/{today}_inventory_test.csv") and exists(f"./Input/{today}_config_test.csv") and exists(f"./Input/{today}_SNMP"):
+    exit()
+if exists(f"./Input/{today}_inventory_test.csv") and exists(f"./Input/{today}_config_test.csv"):
+    inventory_files_in_chunks(today, chunksize, n, baseDir)
+    create_mib_from_inventory(baseDir)
+    device_config_in_chunks(today,baseDir)
+    create_metadata(baseDir)
+    zipdir(baseDir)
+    # upload()
+elif exists(f"./Input/{today}_SNMP"):
+    inventory_files_in_chunks_SNMP(chunksize, n, baseDir)
+    create_metadata(baseDir)
+    zipdir(baseDir)
+#     # upload()
+else:
+    exit()
 
-print("Data Processing Complete")
+print("Data processing completed.")
+
+
